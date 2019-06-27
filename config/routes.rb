846da2fc4287fc6e63c02399ac69a573f3d082bf
isaacmabueno <isaacmabueno@gmail.com>
2019-06-26 20:05:29 -0500

    first

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # get 'ideas/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # add ideas as a resource
+  resources :ideas
+end
---
+++ db/seeds.rb
@@ -0,0 +1,21 @@
+ideas = Idea.create(
+    [
+        {
+            title: 'George Costanza',
+            body: 'My life is the complete opposite of everything I want it to be.'
+        },
+        {
+            title: 'George Costanza',
+            body: 'If she can\'t find me, she can\'t break up with me.'
+        },
+        {
+            title: 'George Costanza',
+            body: 'Don\'t you ever just get down on your knees and thank God that you know me and have access to my dementia?'
+        },
+        {
+            title: 'George Costanza',
+            body: 'The sea was angry that day, my friends, like an old man trying to send back soup in a deli.'
+        },
+
+    ]
+)
